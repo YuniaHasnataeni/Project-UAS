8082af270a08ae50ca06489b8f1db5c17872a090
YuniaHasnataeni <158244192+YuniaHasnataeni@users.noreply.github.com>
2024-06-05 09:48:46 +0700

    Update scrapping_pubmed.R

--- scrapping_pubmed.R
+++ scrapping_pubmed.R
@@ -2,28 +2,22 @@ library(rvest)
 library(httr)
 library(mongolite)
 
-# Fungsi untuk membaca nomor halaman terakhir dari file
-read_last_page <- function() {
-  if (file.exists("last_page.txt")) {
-    return(as.integer(readLines("last_page.txt")))
-  } else {
-    return(1)
-  }
-}
+# URL halaman PubMed dengan pencarian "SVM"
+url <- "https://pubmed.ncbi.nlm.nih.gov/?term=SVM"
 
-# Fungsi untuk menyimpan nomor halaman terakhir ke file
-write_last_page <- function(page_number) {
-  writeLines(as.character(page_number), "last_page.txt")
-}
-
-# Fungsi untuk melakukan scraping
+# Membuat fungsi untuk melakukan scraping
 scrape_pubmed <- function(url) {
+  # Mendapatkan konten HTML dari URL
   page <- GET(url)
-  if (status_code(page) == 200) {
+  # Memeriksa apakah permintaan berhasil
+  if (status_code(page) == 300) {
+    # Mendapatkan konten HTML
     page_content <- read_html(content(page, "text"))
-    titles <- page_content %>% html_nodes(".docsum-title") %>% html_text(trim = TRUE) %>% head(10)
-    links <- page_content %>% html_nodes(".docsum-title") %>% html_attr("href") %>% head(10)
-    authors <- page_content %>% html_nodes(".docsum-authors") %>% html_text(trim = TRUE) %>% head(10)
+    # Mengambil judul, link, dan penulis
+    titles <- page_content %>% html_nodes(".docsum-title") %>% html_text()
+    links <- page_content %>% html_nodes(".docsum-title") %>% html_attr("href")
+    authors <- page_content %>% html_nodes(".docsum-authors") %>% html_text()
+    # Menggabungkan hasil dalam dataframe
     data <- data.frame(
       title = titles,
       author = authors,
@@ -37,38 +31,22 @@ scrape_pubmed <- function(url) {
   }
 }
 
-# Membaca nomor halaman terakhir
-last_page <- read_last_page()
-
-# URL halaman PubMed dengan pencarian "SVM" dan halaman tertentu
-url <- paste0("https://pubmed.ncbi.nlm.nih.gov/?term=SVM&page=", last_page)
-
 # Memanggil fungsi untuk melakukan scraping
 pubmed_data <- scrape_pubmed(url)
-if (!is.null(pubmed_data)) {
-  print(pubmed_data)
-  
-  # MONGODB
-  message('Input Data to MongoDB Atlas')
-  
-  # Membuat koneksi ke MongoDB Atlas
-  atlas_conn <- mongo(
-    collection = Sys.getenv("ATLAS_COLLECTION"),
-    db         = Sys.getenv("ATLAS_DB"),
-    url        = Sys.getenv("ATLAS_URL")
-  )
-  
-  # Memasukkan data ke MongoDB Atlas
-  atlas_conn$insert(pubmed_data)
-  
-  # Menutup koneksi setelah selesai
-  rm(atlas_conn)
-  
-  # Memperbarui nomor halaman terakhir
-  write_last_page(last_page + 1)
-} else {
-  print("Tidak ada data untuk dimasukkan ke MongoDB.")
-}
 
-# Membuang variabel yang tidak diperlukan
-rm(url, scrape_pubmed, last_page, pubmed_data, read_last_page, write_last_page)
+# Melihat hasil scraping
+print(pubmed_data)
+
+# Membuang koneksi
+rm(url, scrape_pubmed)
+
+# MONGODB
+message('Input Data to MongoDB Atlas')
+atlas_conn <- mongo(
+  collection = Sys.getenv("ATLAS_COLLECTION"),
+  db         = Sys.getenv("ATLAS_DB"),
+  url        = Sys.getenv("ATLAS_URL")
+)
+
+atlas_conn$insert(pubmed_data)
+rm(atlas_conn)
